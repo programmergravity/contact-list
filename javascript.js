48b65fd3e8c8f223797a2f8f1964dc1343267897
Kevin Bolaños <programmerslowgravity@gmail.com>
2023-06-26 22:00:28 -0500

    Modificando funciones para agregar datos al contact list

--- javascript.js
+++ javascript.js
@@ -1,25 +1,48 @@
 //          Lista de contactos -1
 
-//  1 Creando lista de contactos
+//  Creando lista de contactos
 
-const listaDeContactos = ['Liliana Martinez', 'Oscar Perez', 'Carlos Gonzales', 'Richard Cuellar', 'Sara Piedrahita'];
+const contactList = [];
 
-//  2 Añadir un contacto
+//  Crear funcion para crear un contacto
 
-function agregarUnContacto() {
-}   agregarUnContacto = listaDeContactos.push('Kevin Bolaños');
-
-//  3 Eliminar un contacto
-
-function eliminarUnContacto() {
-}   listaDeContactos.splice(2, 1);
-
-//  4 Mostrar en consola el resultado
-
-function Contactos(mostrarContactos) {
-    return(mostrarContactos)
+function createContact(id, nombres, apellidos, telefono, ciudad, direccion) {
+    let newContact = {
+      id: id,
+      nombres: nombres,
+      apellidos: apellidos,
+      telefono: telefono,
+      ubicaciones: {
+        ciudad: ciudad,
+        direccion: direccion
+      }
+    };
+    contactList.push(newContact); //   Agregar contactos
 }
 
-const resultado = Contactos(listaDeContactos, agregarUnContacto, eliminarUnContacto);
+//  Crear contactos
 
-console.log(`Mostrando la lista de Contactos`, resultado);
+createContact(4576, 'Julian', 'Ramirez', '3125679000', 'Bogota D.C', 'Avenida boulevard 5500');
+createContact(7896, 'Liliana', 'Martinez', '3175668000', 'Pereira', 'Avenida Pasoancho 6677')
+createContact(3465, 'Kevin', 'Bolaños', '3147678000', 'Mendoza', 'Calle San Martin 4567')
+createContact(8972, 'Andres', 'Tamayo', '3137656000', 'Cali', 'Avenida Simon Bolivar 3422')
+
+console.log(contactList);
+
+//  Eliminar un contacto
+
+function eliminateContact(id) {
+    let indice = contactList.findIndex(function(contacto) {
+      return contacto.id === id;
+    });
+    if (indice !== -1) {
+      contactList.splice(indice, 1);
+    }
+  }
+
+  eliminateContact(7896);
+
+//  Mostrar lista final de contactos
+console.log(contactList);
+
+
